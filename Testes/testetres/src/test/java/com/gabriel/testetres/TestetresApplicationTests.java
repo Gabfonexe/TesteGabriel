b8107d63533cee5b26686c03ba0e0c46eedef09b
Gabfonexe <bielfrancisco23@gmail.com~>
2024-08-30 21:28:18 -0300

    teste 3

---
+++ Testes/testetres/src/test/java/com/gabriel/testetres/TestetresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.testetres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestetresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
