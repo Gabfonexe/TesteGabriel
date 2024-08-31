58f74bf99ba6da5ebcb3030efe59e1351b08f311
Gabfonexe <bielfrancisco23@gmail.com~>
2024-08-30 21:34:33 -0300

    att

--- Testes/testetres/src/main/java/com/gabriel/model/Distribuidora.java
+++ Testes/testetres/src/main/java/com/gabriel/model/Distribuidora.java
@@ -1,5 +1,25 @@
 package com.gabriel.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Distribuidora {
+
+
+  @Id
+  @GeneratedValue( strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private int dia;
+
+  private double valor;
+  
   
 }
